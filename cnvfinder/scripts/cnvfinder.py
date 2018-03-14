703184a4e87d1fa549cb185d000331f2a5c853cd
Andressa Valengo <valengoandressa@gmail.com>
2018-03-14 12:00:39 -0300

    calling method 'command'

--- cnvfinder/scripts/cnvfinder.py
+++ cnvfinder/scripts/cnvfinder.py
@@ -26,7 +26,7 @@ class CNVFinder(object):
         self.args = None
         parser = create_parser('CNVfinder is a Python 3.x package for copy number (CNV) '
                                'variation detection on whole exome sequencing (WES) data from '
-                               'amplicon-based enrichment technologies.',
+                               'amplicon-based enrichment technologies',
                                usage='''cnvfinder <command> [<args>]
                                
 Available commands used in various situations:
@@ -56,20 +56,24 @@ For getting help of a specific command use: cnvfinder <command> --help
         if not hasattr(self, args.command):
             sys.exit('Unrecognized {} command'.format(args.command))
 
+        # use dispatch pattern to invoke method with same name
+        getattr(self, args.command)()
+
     def bedloader(self):
-        parser = create_parser('Preprocessing amplicons defined in a BED file.')
-        parser.add_argument('--target', type=str,
-                            help='Path to file in which sequencing amplicons are listed.')
+        parser = create_parser('Preprocess amplicons defined in a BED file',
+                               usage='''cnvfinder bedloader [<args>]''')
+        parser.add_argument('--target', type=str, required=True,
+                            help='Path to file in which sequencing amplicons are listed')
         parser.add_argument('--region', type=str,
                             help='Limit target definition to a given region. It should be in the form: '
                                  'chr1:10000-90000')
         parser.add_argument('--spacing', type=int, default=20,
                             help='Number of nucleotides to ignore at amplicon start and end, to avoid '
-                                 'overlapping reads.')
+                                 'overlapping reads')
         parser.add_argument('--min-data', type=int, default=50,
-                            help='Minimum number of nucleotides for a valid target.')
+                            help='Minimum number of nucleotides for a valid target')
         parser.add_argument('--max-pool', type=int, default=None,
-                            help='Maximum number of origin pools allowed for a valid target.')
+                            help='Maximum number of origin pools allowed for a valid target')
 
         self.args = parser.parse_args(sys.argv[2:])
 
