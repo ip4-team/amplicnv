9731a6cb4cf9ea8a0a23013a9f5d7fc87875602a
Andressa Valengo <valengoandressa@gmail.com>
2018-10-18 14:34:27 -0300

    updated version and added bedhandler dependency

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0b4',
+    version='1.0.0b5',
 
     description='A Python3.x package for CNV detection',
     long_description=long_description,
@@ -76,7 +76,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pysam', 'pandas', 'plotly', 'numpy', 'scikit-learn', 'scipy', 'numexpr', 'matplotlib'],
+    install_requires=['pysam', 'pandas', 'plotly', 'numpy', 'scikit-learn', 'scipy', 'numexpr', 'matplotlib', 'bedhandler'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
